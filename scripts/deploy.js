7ddcd40307135ec4b209dfa2b77f4d1f6aa64c17
raiyanmook27 <raiyan.mook27@gmail.com>
2022-10-04 13:06:01 +0000

    FEAT: FIXED THE ABI BUG

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,18 +1,24 @@
+// Import ethers from Hardhat package
 const { ethers } = require("hardhat");
 
 async function main() {
+  /*
+    A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+    so whitelistContract here is a factory for instances of our Whitelist contract.
+*/
   const whitelistContract = await ethers.getContractFactory("Whitelist");
 
-  const deployWhitelistContract = await whitelistContract.deploy(10);
+  // here we deploy the contract
+  const deployedWhitelistContract = await whitelistContract.deploy(10);
 
-  await deployWhitelistContract.deployed();
-
-  console.log("Whitelist Contract Address:", deployWhitelistContract.address);
+  // print the address of the deployed contract
+  console.log("Whitelist Contract Address:", deployedWhitelistContract.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
